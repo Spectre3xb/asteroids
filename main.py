73d9b558fce92289f60ca2a626d837fdc0b039e6
Spectre3xb <stefan.g.buchholz@gmail.com>
2025-09-03 23:14:02 +0200

    60 fps

--- main.py
+++ main.py
@@ -11,12 +11,15 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
     while game:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = (clock.tick(60)) / 1000
 
 if __name__ == "__main__":
     main()
